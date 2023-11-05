308be228fa1f5bd358b0ac465ce2dba9a26bd66c
Everyhongcha <80094147+everyt@users.noreply.github.com>
2023-11-05 22:41:35 +0900

    Add modal on mouse & href on stacks

--- components/view/ContactView.tsx
+++ components/view/ContactView.tsx
@@ -2,9 +2,37 @@
 
 import Image from 'next/image';
 import { motion, useAnimationControls } from 'framer-motion';
+import { useEffect, useState } from 'react';
 
 export default function ContactView() {
   const controls = useAnimationControls();
+
+  const [onHoverName, setOnHoverName] = useState(false);
+  const [onHoverGitLink, setOnHoverGitLink] = useState(false);
+  const [onHoverEmail, setOnHoverEmail] = useState(false);
+
+  const [cursorPosition, setCursorPosition] = useState({ x: 0, y: 0 });
+
+  useEffect(() => {
+    const handleMousePosition = (e: MouseEvent) => {
+      setCursorPosition({ x: e.clientX, y: e.clientY });
+    };
+    window.addEventListener('mousemove', handleMousePosition);
+    return () => window.removeEventListener('mousemove', handleMousePosition);
+  }, []);
+
+  const cursorVariants = {
+    default: {
+      opacity: 1,
+      scale: 1,
+      transition: {
+        duration: 0.001,
+        ease: 'linear',
+      },
+      x: cursorPosition.x,
+      y: cursorPosition.y - 30,
+    },
+  };
 
   const animation = {
     scale: 1.1,
@@ -38,7 +66,7 @@ export default function ContactView() {
 
   const getButton = (obj: any) => {
     return (
-      <motion.div key={obj.text} className={`${baseCSS}`} whileHover={animation}>
+      <motion.div key={obj.text} className={`${baseCSS}`} whileHover={animation} onClick={() => hrefTo(obj.href)}>
         <div className={`rounded-md overflow-hidden ${obj.adobe}`}>
           <Image src={`/stacks/${obj.text}.svg`} height={20} width={20} alt='' />
         </div>
@@ -56,22 +84,66 @@ export default function ContactView() {
 
   return (
     <>
+      {onHoverName && (
+        <motion.div
+          className={`${baseCSS} absolute left-0 top-0 text-sm z-[500]`}
+          variants={cursorVariants}
+          animate='default'>
+          <span>Everyt</span>
+        </motion.div>
+      )}
+      {onHoverGitLink && (
+        <motion.div
+          className={`${baseCSS} absolute left-0 top-0 text-sm z-[500]`}
+          variants={cursorVariants}
+          animate='default'>
+          <span>새 창에서 깃허브 링크를 엽니다.</span>
+        </motion.div>
+      )}
+      {onHoverEmail && (
+        <motion.div
+          className={`${baseCSS} absolute left-0 top-0 text-sm z-[500]`}
+          variants={cursorVariants}
+          animate='default'>
+          <span>클릭하면 클립보드에 복사됩니다.</span>
+        </motion.div>
+      )}
       <main className='flex w-[80vw] h-[90vh] flex-col items-start justify-center py-24 pt-32 md:pt-8'>
-        <span className='font-extrabold text-xl mb-2'>곽태웅</span>
-        <span className='cursor-pointer' onClick={() => hrefTo('https://github.com/everyt')}>
-          https://github.com/everyt
+        <span
+          className='font-extrabold text-xl mb-2'
+          onMouseEnter={() => setOnHoverName(true)}
+          onMouseLeave={() => setOnHoverName(false)}>
+          곽태웅
         </span>
+        <div className='flex items-center'>
+          <span
+            className='cursor-pointer'
+            onClick={() => hrefTo('https://github.com/everyt')}
+            onMouseEnter={() => setOnHoverGitLink(true)}
+            onMouseLeave={() => setOnHoverGitLink(false)}>
+            https://github.com/everyt
+          </span>
+        </div>
         <input id='toCopy' type='hidden' value='everytea.time@gmail.com' readOnly />
-        <motion.span
-          id='email'
-          className='mb-3 cursor-pointer'
-          onClick={copyEmail}
-          animate={controls}
-          variants={variants}>
-          everytea.time@gmail.com
-        </motion.span>
-        <div className='flex mt-3 justify-center'>
-          {[{ text: 'Javascript' }, { text: 'Typescript' }, { text: 'Java' }].map((obj) => getButton(obj))}
+        <div className='flex items-center'>
+          <motion.span
+            id='email'
+            className='cursor-pointer'
+            onClick={copyEmail}
+            animate={controls}
+            variants={variants}
+            onMouseEnter={() => setOnHoverEmail(true)}
+            onMouseLeave={() => setOnHoverEmail(false)}>
+            everytea.time@gmail.com
+          </motion.span>
+        </div>
+        <div className='flex mt-6 justify-center'>
+          {[
+            { text: 'Javascript', href: 'https://en.wikipedia.org/wiki/JavaScript' },
+            { text: 'Node.js', href: 'https://nodejs.org/' },
+            { text: 'Typescript', href: 'https://www.typescriptlang.org/' },
+            { text: 'Java', href: 'https://www.oracle.com/java/' },
+          ].map((obj) => getButton(obj))}
         </div>
         <div className='flex justify-center'>
           {[
@@ -80,17 +152,28 @@ export default function ContactView() {
               textColor: 'text-sky-800',
               secondText: 'SQL',
               secondColor: 'text-orange-400',
+              href: 'https://www.mysql.com/',
             },
-            { text: 'MariaDB' },
-            { text: 'FireBase' },
+            { text: 'MariaDB', href: 'https://mariadb.org/' },
+            { text: 'FireBase', href: 'https://firebase.google.com/' },
           ].map((obj) => getButton(obj))}
         </div>
         <div className='flex justify-center'>
           {[
-            { text: 'React', textColor: 'text-sky-500' },
-            { text: 'Next.js' },
-            { text: 'Svelte', secondText: 'Kit', secondColor: 'text-slate-400' },
-            { text: 'Spring', secondText: 'Boot', secondColor: 'text-lime-700' },
+            { text: 'React', textColor: 'text-sky-500', href: 'https://legacy.reactjs.org/' },
+            { text: 'Next.js', href: 'https://nextjs.org/' },
+            {
+              text: 'Svelte',
+              secondText: 'Kit',
+              secondColor: 'text-slate-400',
+              href: 'https://kit.svelte.dev/',
+            },
+            {
+              text: 'Spring',
+              secondText: 'Boot',
+              secondColor: 'text-lime-700',
+              href: 'https://spring.io/projects/spring-boot',
+            },
           ].map((obj) => getButton(obj))}
         </div>
         <div className='flex justify-center'>
@@ -99,26 +182,36 @@ export default function ContactView() {
               text: 'Premiere Pro',
               textColor: 'text-purple-950',
               adobe: 'bg-purple-300',
+              href: 'https://www.adobe.com/products/premiere.html',
             },
             {
               text: 'After Effects',
               textColor: 'violet-950',
               adobe: 'bg-violet-300',
+              href: 'https://www.adobe.com/products/aftereffects.html',
             },
           ].map((obj) => getButton(obj))}
         </div>
         <div className='flex justify-center'>
           {[
-            { text: 'Photoshop', textColor: 'text-sky-950', adobe: 'bg-sky-400' },
+            {
+              text: 'Photoshop',
+              textColor: 'text-sky-950',
+              adobe: 'bg-sky-400',
+              href: 'https://www.adobe.com/products/photoshop.html',
+            },
             {
               text: 'Illustrator',
               textColor: 'text-orange-950',
               adobe: 'bg-orange-400',
+              href: 'https://www.adobe.com/products/illustrator.html',
             },
           ].map((obj) => getButton(obj))}
         </div>
         <div className='flex justify-center'>
-          {[{ text: 'Blender', textColor: 'text-sky-700' }].map((obj) => getButton(obj))}
+          {[{ text: 'Blender', textColor: 'text-sky-700', href: 'https://www.blender.org/' }].map((obj) =>
+            getButton(obj),
+          )}
         </div>
       </main>
     </>
